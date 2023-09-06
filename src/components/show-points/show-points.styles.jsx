013d16768544e5bb169a4a3a50725c605168c123
Ivan Knezevic <ivan.knezevic@wackes.com>
2023-09-06 22:40:31 +0200

    D

--- src/components/show-points/show-points.styles.jsx
+++ src/components/show-points/show-points.styles.jsx
@@ -1,48 +1,44 @@
 import { styled } from "styled-components";
 
 export const PointsContainer = styled.div`
-   margin-top: 50px;
-   display: flex;
-   min-height: 150px;
-   flex-direction: column;
-   width: 100vw;
-   align-items: center;
+    margin-top: 50px;
+    display: flex;
+    min-height: 150px;
+    flex-direction: column;
+    width: 100vw;
+    align-items: center;
 `;
 
 export const Name = styled.p`
-   display: inline-block;
-   text-align: center;
-   font-size: 20px;
-   font-weight: 900;
+    display: inline-block;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 900;
 
-   @media (max-width: 900px) {
-         font-size: 16px;
-      text-align: left;
-   }
-
+    @media (max-width: 900px) {
+        font-size: 16px;
+        text-align: left;
+    }
 `;
-
 
 export const Point = styled.p`
-   display: inline-block;
-   text-align: center;
-   font-size: 20px;
-   font-weight: 900;
+    display: inline-block;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 900;
 
-   @media (max-width: 900px) {
-         font-size: 16px;
-      text-align: left;
-   }
-
+    @media (max-width: 900px) {
+        font-size: 16px;
+        text-align: left;
+    }
 `;
 
-
 export const PointsInfo = styled.div`
-   position: relative;
-   width: 70%;
-   display: flex;
-   justify-content: space-between;
-   @media (min-width: 900px) {
-      width: 50%;
-   }
+    position: relative;
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    @media (min-width: 900px) {
+        width: 50%;
+    }
 `;
