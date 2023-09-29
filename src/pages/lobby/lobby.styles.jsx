7d5510f2f6bc427d36fc6e1414eb2d9e53a69ac1
Ivan Knezevic <ivan.k@live.se>
2023-09-29 16:13:57 +0200

    CSS fix

--- src/pages/lobby/lobby.styles.jsx
+++ src/pages/lobby/lobby.styles.jsx
@@ -1,52 +1,51 @@
 import styled from "styled-components";
 
 export const LobbyContainer = styled.div`
-   width: 100vw;
-   display: flex;
-   flex-direction: column;
-   justify-content: space-between;
-   h2 {
-      justify-content: center;
-      text-align: center;
-      justify-self: flex-end;
-   }
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h2 {
+    justify-content: center;
+    text-align: center;
+    justify-self: flex-end;
+  }
 `;
 export const PlayersContainer = styled.div`
-   display: flex;
-   flex-direction: column;
-   
-   min-height: 80vh;
-   max-height: 80vh;
-   width: 100vw;
-   align-items:center;
-   overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
 
-   @media (max-width: 900px) {
-      margin-top: 50px;
-   }
+  min-height: 70vh;
+  max-height: 70vh;
+  width: 100vw;
+  align-items: center;
+  overflow-y: scroll;
+
+  @media (max-width: 900px) {
+    margin-top: 50px;
+  }
 `;
 export const GameKey = styled.h1`
-   position: relative;
-   align-self: center;
-   text-align: center;
-   margin: 0px;
-   font-size: 70px;
+  position: fixed;
+  top: 20px;
+  align-self: center;
+  text-align: center;
+  margin: 0px;
+  font-size: 70px;
 
-      
-   @media (max-width: 900px) {
-      top: 20px;
-      font-size: 50px;
-   }
+  @media (max-width: 900px) {
+    top: 20px;
+    font-size: 50px;
+  }
 `;
 
 export const PlayerCounter = styled.div`
-   width: 100%;
-   box-sizing: border-box;
-`
-export const Info = styled.h2`
-
-`
+  width: 100%;
+  box-sizing: border-box;
+`;
+export const Info = styled.h2``;
 
 export const BottomItems = styled.div`
-
-`
+  position: fixed;
+  bottom: 10px;
+`;
